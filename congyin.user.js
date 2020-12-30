66f853e540d3e1d7fb5e7a92924f2cf5c4a7f80f
CGYG <2428092039@qqcom>
2020-12-30 09:50:15 +0800

    Signed-off-by: CGYG <2428092039@qqcom> 大麦网订单确认插件第一版

---
+++ congyin.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         大麦网抢票插件—1
+// @namespace    https://www.congyin.top/
+// @version      0.2
+// @description  通过插件成功抢票
+// @author       Congyin
+// @match        https://detail.damai.cn/item.htm?*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.11.1.min.js
+
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
